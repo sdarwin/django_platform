287b7fde80d3bd7823aa513662046b12997620ba
Caleb Severn <calsev@gmail.com>
2018-08-12 09:26:59 -0800

    Updated user logic to be system user

--- recipes/user.rb
+++ recipes/user.rb
@@ -2,7 +2,11 @@
 
 tcb = 'se_django_app'
 
-user 'django'
+user 'django' do
+  system true
+  shell '/usr/sbin/nologin'
+  manage_home false # Owned resources are in /opt
+end
 
 group 'django' do
   members ['django', node[tcb]['apache_user']]
