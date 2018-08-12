596bd05eeca77666ea2e2d9f756aab02a3594ae0
Caleb Severn <calsev@gmail.com>
2018-08-12 09:27:47 -0800

    Added logic to install python runtime

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# '3' works in CentOS 7 but not Ubuntu 18 as of August 2018
+if platform_family?('debian')
+  python_runtime '3' do
+    options package_name: 'python3'
+  end
+else
+  python_runtime '3'
+end
