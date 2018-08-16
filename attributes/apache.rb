0b3f5557a86188fe30a8b269bef7b9da16f6cf65
Caleb Severn <calsev@gmail.com>
2018-08-15 16:28:47 -0800

    Scoped apache attribute

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -3,10 +3,10 @@
 tcb = 'se_django_app'
 
 default[tcb]['apache']['service_name'] = if platform_family?('debian')
-                                   'apache2'
-                                 else
-                                   'httpd'
-                                 end
+                                           'apache2'
+                                         else
+                                           'httpd'
+                                         end
 
 # mod_ssl and mod_wsgi are always installed; use this for additional Apache2 modules
 # See https://github.com/sous-chefs/apache2#recipes for a list of modules
