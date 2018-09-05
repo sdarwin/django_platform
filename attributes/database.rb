b907dc2993177bda6571774951ee704d264ecaa2
Caleb Severn <calsev@gmail.com>
2018-09-04 20:16:19 -0800

    Now fetching a passowrd for the database

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
 tcb = 'se_django_app'
+
+# Password for DB
+default[tcb]['db_password']['vault_data_bag'] = 'passwords' # The name of the vault data bag
+default[tcb]['db_password']['vault_bag_item'] = 'django_db' # item inside the data bag (json file)
+default[tcb]['db_password']['vault_item_key'] = 'password' # The key for password within the json object
--- recipes/database.rb
+++ recipes/database.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
 tcb = 'se_django_app'
+
+# Will raise 404 error if not found
+db_secret = chef_vault_item(
+  node[tcb]['db_password']['vault_data_bag'],
+  node[tcb]['db_password']['vault_bag_item']
+)
+raise 'Unable to retrieve data bag for SSH key' if db_secret.nil?
+db_password = db_secret[node[tcb]['db_password']['vault_item_key']]
+raise 'Unable to retrieve SSH key' if db_password.nil?
