3c28738865b3094dcf98d287b21a165581d197ab
Caleb Severn <calsev@gmail.com>
2018-10-02 12:47:14 -0800

    Added install of postgresql

--- recipes/database.rb
+++ recipes/database.rb
@@ -10,3 +10,7 @@ db_secret = chef_vault_item(
 raise 'Unable to retrieve data bag for SSH key' if db_secret.nil?
 db_password = db_secret[node[tcb]['db_password']['vault_item_key']]
 raise 'Unable to retrieve SSH key' if db_password.nil?
+
+postgresql_server_install 'Install PostgreSQL' do
+  password db_password
+end
