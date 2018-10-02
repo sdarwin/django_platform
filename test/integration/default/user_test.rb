107b1052d8940428b16598634d667722193f693e
Caleb Severn <calsev@gmail.com>
2018-10-02 11:55:18 -0800

    Added tests for apache user

--- test/integration/default/user_test.rb
+++ test/integration/default/user_test.rb
@@ -9,5 +9,13 @@ describe user('django') do
   its('group') { should eq 'django' }
   its('groups') { should eq ['django'] }
   its('home') { should eq '/home/django' }
-  its('shell') { should eq '/bin/nologin' }
+  its('shell') { should eq '/usr/sbin/nologin' }
+end
+
+describe user(apache_user(node)) do
+  it { should exist }
+  its('group') { should eq apache_user(node) }
+  its('groups') { should eq [apache_user(node), 'django'] }
+  its('home') { should eq apache_home_dir(node) }
+  its('shell') { should eq apache_shell(node) }
 end
--- test/integration/helpers.rb
+++ test/integration/helpers.rb
@@ -1,1 +1,34 @@
 # frozen_string_literal: true
+
+def apache_user(node)
+  if node['platform_family'] == 'debian'
+    user = 'root'
+  elsif node['platform_family'] == 'rhel'
+    user = 'apache'
+  else
+    raise "Platform family not recognized: #{node['platform_family']}"
+  end
+  return user
+end
+
+def apache_home_dir(node)
+  if node['platform_family'] == 'debian'
+    dir = '/root'
+  elsif node['platform_family'] == 'rhel'
+    dir = '/usr/share/httpd'
+  else
+    raise "Platform family not recognized: #{node['platform_family']}"
+  end
+  return dir
+end
+
+def apache_shell(node)
+  if node['platform_family'] == 'debian'
+    shell = '/bin/bash'
+  elsif node['platform_family'] == 'rhel'
+    shell = '/sbin/nologin'
+  else
+    raise "Platform family not recognized: #{node['platform_family']}"
+  end
+  return shell
+end
