dac6e9a50d3df522734f1c71278e093051394f81
Caleb Severn <calsev@gmail.com>
2018-10-02 20:35:05 -0800

    Added a test for virtual environment

--- test/integration/default/python_test.rb
+++ test/integration/default/python_test.rb
@@ -8,3 +8,11 @@ describe package(python_package(node)) do
   it { should be_installed }
   its(:version) { should match '^3.6' }
 end
+
+describe file('/home/django/env/pyvenv.cfg') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'django' }
+  it { should be_grouped_into 'django' }
+end
