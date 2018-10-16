5127b3ff27329f56d4b8062a69e4cbbbdee16bd1
Caleb Severn <calsev@gmail.com>
2018-10-16 15:09:25 -0800

    Added provision for updating system pip

--- attributes/python.rb
+++ attributes/python.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 tcb = 'django_platform'
+
+# Pip 18.1 broke poise-python
+# Set to true for the latest
+default['poise-python']['options']['pip_version'] = '18.0'
--- recipes/python.rb
+++ recipes/python.rb
@@ -10,9 +10,9 @@ include_recipe 'yum-epel::default'
 # We should use a virtual environment, but poise-python is busted as of pip 18.1 release
 python_runtime '3' do
   options package_name: python_package_name
-  pip_version false
-  setuptools_version false
-  wheel_version false
+  pip_version false if busted_poise? # This cannot be true because that overrides the attribute
+  setuptools_version false if busted_poise?
+  wheel_version false if busted_poise?
 end
 package python_package_prefix + 'pip' if busted_poise?
 package python_package_prefix + 'setuptools' if busted_poise?
