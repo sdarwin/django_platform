9d0b44595fc60a031b86001283ef8b7fd0fb852f
Caleb Severn <calsev@gmail.com>
2018-12-21 12:17:17 -0900

    Moved known hosts test to site suites

--- test/integration/default/app_test.rb
+++ test/integration/default/app_test.rb
@@ -4,15 +4,6 @@ require_relative '../helpers'
 
 node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
 
-describe file('/home/django/.ssh/known_hosts') do
-  it { should exist }
-  it { should be_file }
-  it { should be_mode 0o644 }
-  it { should be_owned_by 'django' }
-  it { should be_grouped_into 'django' }
-  its(:content) { should match(/github\.alaska\.edu ssh-rsa/) }
-end
-
 describe file("#{path_to_conf_directory(node)}/django.conf") do
   it { should exist }
   it { should be_file }
