110eb749b45dc1a3362580ce76948a04730cb8c6
Caleb Severn <calsev@gmail.com>
2018-12-31 12:14:20 -0900

    Adopted tcp paradigm for recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+tcb = 'django_platform'
+
 node.default['http_platform']['www']['document_root'] = File.join(path_to_app_repo, rel_path_to_http_root)
 
 include_recipe 'http_platform::default'
 
-include_recipe 'django_platform::user'
+include_recipe "#{tcb}::user"
 
-include_recipe 'django_platform::database'
+include_recipe "#{tcb}::database"
 
-include_recipe 'django_platform::python'
+include_recipe "#{tcb}::python"
 
-include_recipe 'django_platform::app'
+include_recipe "#{tcb}::app"
