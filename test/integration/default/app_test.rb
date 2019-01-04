831d315dc6a8be3f324c8753989db33a5925122d
Caleb Severn <calsev@gmail.com>
2019-01-04 12:39:37 -0900

    Eliminated redundant test

--- test/integration/default/app_test.rb
+++ test/integration/default/app_test.rb
@@ -10,7 +10,7 @@ describe file('/home/django/.ssh/known_hosts') do
   it { should be_mode 0o644 }
   it { should be_owned_by 'django' }
   it { should be_grouped_into 'django' }
-  its(:content) { should match('github\.com ssh-rsa') }
+  # Content checked in app tests
 end
 
 describe file('/opt/chef/run_record/django_sentinel.txt') do
