021c4f308e7f1d24286fef998df625a89637fc30
Caleb Severn <calsev@gmail.com>
2019-01-04 11:30:52 -0900

    Added test for epel repo

--- test/integration/default/python_test.rb
+++ test/integration/default/python_test.rb
@@ -4,6 +4,15 @@ require_relative '../helpers'
 
 node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
 
+describe bash('yum repolist') do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq '' }
+  its(:stdout) { should match 'epel/x86_64' }
+  before do
+    skip if node['platform_family'] == 'debian'
+  end
+end
+
 describe package(python_package(node)) do
   it { should be_installed }
   its(:version) { should match '^3.6' }
