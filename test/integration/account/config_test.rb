eae5cd3d56db5bae26da2ff42a26434b424cd098
Caleb Severn <calsev@gmail.com>
2019-01-10 10:33:16 -0900

    Updated config test

--- test/integration/account/config_test.rb
+++ test/integration/account/config_test.rb
@@ -2,14 +2,18 @@
 
 require_relative '../helpers'
 
-describe file('/home/django/repo/app/shared/conf/config.ini') do
+describe file('/home/django/repo/app/shared_app/conf/config.ini') do
   it { should exist }
   it { should be_file }
   it { should be_mode 0o440 }
   it { should be_owned_by 'django' }
   it { should be_grouped_into 'django' }
-  its(:content) { should match(/user = seadmin/) }
-  its(:content) { should match(/host = calsev/) }
-  its(:content) { should match(/user = cjsevern/) }
-  its(:content) { should match(/mail = cjsevern@alaska.edu/) }
+  # rubocop:disable Metrics/LineLength
+  its(:content) { should match(/user = CN=oit account admin,OU=Account Provisioning,OU=Services,OU=SW,DC=ua,DC=ad,DC=alaska,DC=edu/) }
+  # rubocop:enable Metrics/LineLength
+  its(:content) { should match(/base = DC=ua,DC=ad,DC=alaska,DC=edu/) }
+  its(:content) { should match(/instance = prod/) }
+  its(:content) { should match(/username = calsev/) }
+  its(:content) { should match(/username = cjsevern/) }
+  its(:content) { should match(/email_address = cjsevern@alaska.edu/) }
 end
