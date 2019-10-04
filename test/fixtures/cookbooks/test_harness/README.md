dc51bc2a5147df8e714c09ac402fc97d5f5b17e1
Caleb Severn <calsev@gmail.com>
2019-10-04 15:15:20 -0800

    Add test harness

---
+++ test/fixtures/cookbooks/test_harness/README.md
@@ -0,0 +1,1 @@
+Test fixture for the [django_platform](https://github.com/ualaska-it/django_platform) cookbook.
---
+++ test/fixtures/cookbooks/test_harness/attributes/default.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+default['http_platform']['admin_email'] = 'ua-oit-se@alaska.edu'
+default['http_platform']['cert']['organization'] = 'University of Alaska'
+default['http_platform']['cert']['org_unit'] = 'OIT'
+
+default['django_platform']['app_repo']['rel_path_to_manage_directory'] = ''
+default['django_platform']['app_repo']['rel_path_to_site_directory'] = 'default_django_site'
+default['django_platform']['app_repo']['rel_path_to_static_directory'] = 'static'
+default['django_platform']['app_repo']['rel_path_to_sqlite_db'] = 'db.sqlite3'
+
+default['django_platform']['app_repo']['git_user'] = 'calsev'
+default['django_platform']['app_repo']['git_repo'] = 'django_default_site'
