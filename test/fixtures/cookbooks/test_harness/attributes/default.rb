d6f21d524b72e64f3c8f85d7531345e73d512b57
Caleb Severn <calsev@gmail.com>
2019-10-09 09:59:03 -0800

    Fix path to default site

--- test/fixtures/cookbooks/test_harness/attributes/default.rb
+++ test/fixtures/cookbooks/test_harness/attributes/default.rb
@@ -5,7 +5,7 @@ default['http_platform']['cert']['organization'] = 'University of Alaska'
 default['http_platform']['cert']['org_unit'] = 'OIT'
 
 default['django_platform']['app_repo']['rel_path_to_manage_directory'] = ''
-default['django_platform']['app_repo']['rel_path_to_site_directory'] = 'default_django_site'
+default['django_platform']['app_repo']['rel_path_to_site_directory'] = 'django_default_site'
 default['django_platform']['app_repo']['rel_path_to_static_directory'] = 'static'
 default['django_platform']['app_repo']['rel_path_to_sqlite_db'] = 'db.sqlite3'
 
