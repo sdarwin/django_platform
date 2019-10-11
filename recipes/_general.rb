f922a48840d54ef047cd067b5712848139c66df7
Caleb Severn <calsev@gmail.com>
2019-10-11 09:45:39 -0800

    Add apt update before install

--- recipes/_general.rb
+++ recipes/_general.rb
@@ -6,3 +6,20 @@ if platform_family?('rhel')
     action node['selinux']['state'].downcase.to_sym
   end
 end
+
+idemp_file = '/var/chef/idempotence/django_first_run_update'
+
+apt_update 'Pre-Install Update' do
+  action :update
+  not_if { File.exist?(idemp_file) }
+end
+
+directory '/var/chef/idempotence' do
+  user 'root'
+  group 'root'
+  recursive true
+end
+
+file idemp_file do
+  content 'Sentinel'
+end
